afd909f35f887f5e6e835042fcffd408c9ea7f37
sabyasachi de <sabyasachidetcs2021@outlook.com>
2021-04-15 12:53:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int t1 = 0;
+            int t2 = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                string d = "";
+                t1++;
+                t2++;
+                if (t1 == 3)
+                {
+                    d += "fizz";
+                    t1 = 0;
+                }
+                if(t2==5)
+                {
+                    d += "buzz";
+                    t2 = 0;
+
+                }
+                if(d !="")
+                {
+                    Console.WriteLine(d);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+
+                }
+
+                
+              
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
